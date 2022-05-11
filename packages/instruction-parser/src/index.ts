7143dd87a98dc22daa18b5957bde1a191f03559f
Petar Markov <speculees@gmail.com>
2022-05-11 16:25:15 +0200

    chore: send unique parser requests

--- packages/instruction-parser/src/index.ts
+++ packages/instruction-parser/src/index.ts
@@ -1,9 +1,12 @@
 import { ProgramInfo, ParserFactory } from '@solflare-wallet/solana-programs';
 
+type Parser = (data: unknown) => unknown;
 document.body.style.backgroundColor = '#0f0f0faa';
 document.body.style.color = '#00f000';
 const root = document.getElementById('root');
-const parserMap = new Map<string, ({ accounts, data }: { accounts: string[]; data: string | number }) => unknown>();
+const parserMap = new Map<string, Parser>();
+const parserPromises = new Map<string, Promise<Parser>>();
+const parserResolves = new Map<string, Parser>();
 let programMap = new Map<string, ProgramInfo>();
 
 function render(message: string) {
@@ -15,40 +18,65 @@ function render(message: string) {
 	root.innerText += '\n' + message;
 }
 
+/**
+ * @param programId pubkey of the program
+ * @returns `ProgramInfo` object
+ * @description Returns `ProgramInfo` object for the program with the given pubkey. 
+ * If the program is not found, parser that returns `type: 'unknown` will be created in `createParser`.
+ */
+function getProgram(programId: string) {
+	return programMap.get(programId) || ({
+		parserFactory: {
+			repository: null,
+			createParser: async () => () => ({ type: 'unknown' }),
+		} as ParserFactory,
+	} as ProgramInfo);
+}
+
+function getParser(programId: string) {
+	if (parserMap.has(programId)) {
+		return parserMap.get(programId);
+	} else if (parserPromises.has(programId)) {
+		return parserPromises.get(programId);
+	}
+
+	return null;
+}
+
 function subscribeToMessages() {
 	window.addEventListener('message', async (event) => {
 		if (event.data.type === 'instruction') {
 			const { accounts, instruction, id } = event.data.message;
 			const { programId, data, parsed } = instruction;
-			const program = programMap.get(programId) || ({
-				parserFactory: {
-					repository: null,
-					createParser: async () => () => ({ type: 'unknown' }),
-				} as ParserFactory,
-			} as ProgramInfo);
+			const program = getProgram(programId);
 
 			if (parsed) {
 				render('instruction already contains parsed data');
 				program.parsed = parsed;
 				parent.postMessage({ type: 'instruction', message: { id, programId, data: program } }, '*');
 			} else {
-				const parser = parserMap.get(programId);
+				const parser = await getParser(programId);
 
+				// use cached parser if it exists
 				if (parser) {
 					render('using cached parser');
 					program.parsed = parser({ accounts, data });
 					render('parsing data ... done');
-				} else {
+				} else { // fetch parser from repository
 					if (program.parserFactory) {
 						const { parserFactory, hasParser } = program;
 						render('creating parser ...');
 
 						try {
 							if (hasParser) {
-								const parser = await parserFactory.createParser(`/src/programs/${programId}.js`, false);
-								parserMap.set(programId, parser);
+								parserPromises.set(programId, new Promise((resolve) => parserResolves.set(programId, resolve)));
+								const _parser = await parserFactory.createParser(`/src/programs/${programId}.js`, false);
+								parserResolves.get(programId)(_parser);
+								parserResolves.delete(programId);
+								parserPromises.delete(programId);
+								parserMap.set(programId, _parser);
 								render('creating parser ... done');
-								program.parsed = parser({ accounts, data });
+								program.parsed = _parser({ accounts, data });
 							} else {
 								render('program does not have a parser');
 								program.parsed = { type: 'unknown' };
